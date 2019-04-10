e628eefad568868c7b9456f585f2625d3fec1410
danwenzel <dan@danwenzel.com>
2019-04-10 10:33:15 -0500

    Stop running translation downloads on preBuild
    You’ll now have to manually run ember i18n:download first

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line node/no-extraneous-require
 const Funnel = require('broccoli-funnel');
-const fs = require('fs');
-const downloadTranslations = require('./lib/commands/download');
 
 process.on('unhandledRejection', error => {
   throw error;
@@ -10,24 +8,6 @@ process.on('unhandledRejection', error => {
 module.exports = {
   name: require('./package').name,
   excludeFromBuild: false,
-  hasDownloadedTranslations: false,
-
-  preBuild: function() {
-    if (
-      // only download translations on initial build, not on live reloads
-      !this.hasDownloadedTranslations
-      // don't download in development; would slow things down.  Devs should download manually
-      && this.app.env !== 'development'
-      // if this is ember-cli-crowdin itself, there's nothing to download
-      && !this.project.root.includes('ember-cli-crowdin')
-    ) {
-      return this._downloadTranslations().then(() => {
-        this.hasDownloadedTranslations = true;
-      }).catch((error) => {
-        throw error;
-      });
-    }
-  },
 
   postBuild(results) {
     if (
@@ -71,31 +51,5 @@ module.exports = {
       'i18n:setup': require('./lib/commands/setup'),
       'i18n:upload': require('./lib/commands/upload')
     };
-  },
-
-  _downloadTranslations() {
-    // download consuming application's translations
-    const appPromise = downloadTranslations.run.call(this, {
-      project: this.project
-    });
-
-    return appPromise.then(() => {
-      return Promise.all(this._addonsPromises());
-    }).catch((error) => {
-      throw error;
-    });
-  },
-
-  _addonsPromises() {
-    // download all addons' translations
-    const outdoorsyAddonsWithTranslations = this.project.addons.filter((addon) => {
-      const path = addon.root;
-      return path.includes('outdoorsyco') && fs.existsSync(`${path}/config/crowdin.js`);
-    });
-    return outdoorsyAddonsWithTranslations.map((addon) => {
-      downloadTranslations.run.call(this, {
-        project: addon
-      });
-    });
   }
 };
